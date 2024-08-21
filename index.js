cdb09984877ade1f38b810b7dd1b52bee5e65233
TyRoopam9599 <roopamtyagi72@gmail.com>
2024-08-21 11:46:41 +0530

    Bitwise Operator added

--- index.js
+++ index.js
@@ -46,9 +46,97 @@ function divide(a, b) {
     return a / b;
 }
 
+/**
+ * Calculate the result of a number raised to the power of an exponent.
+ * @param {number} base - The base number.
+ * @param {number} exponent - The exponent.
+ * @returns {number} The result of base raised to the power of exponent.
+ */
+function exponential(base, exponent) {
+  return Math.pow(base, exponent);
+}
+
+/**
+ * Calculates the modulus (remainder) of two numbers.
+ *
+ * @param {number} a - The dividend.
+ * @param {number} b - The divisor.
+ * @returns {number} The remainder after dividing `a` by `b`.
+ */
+function modulus(a, b) {
+    return a % b;
+}
+
+/**
+ * Performs a bitwise AND operation on two integers.
+ *
+ * @param {number} a - The first integer.
+ * @param {number} b - The second integer.
+ * @returns {number} The result of the bitwise AND operation.
+ */
+function bitwiseAnd(a, b) {
+    return a & b;
+}
+
+function bitwiseOr(a, b){
+    return a | b ;
+}
+
+function bitwiseXor(a, b){
+    return a ^ b ;
+}
+
+/**
+ * Performs a bitwise NOT operation on the given integer.
+ *
+ * @param {number} a - The integer to perform the bitwise NOT operation on.
+ * @returns {number} The result of the bitwise NOT operation.
+ */
+function bitwiseNot(a) {
+    return ~a;
+}
+
+/**
+ * Performs a left bitwise shift on the first operand by the number of bits specified by the second operand.
+ *
+ * @param {number} a - The number to be shifted.
+ * @param {number} b - The number of bits to shift `a` to the left.
+ * @returns {number} The result of shifting `a` to the left by `b` bits.
+ */
+function leftShift(a, b) {
+    return a << b;
+}
+
+function rightShift(a, b){
+    return a >> b ;
+}
+
+/**
+ * Performs a zero-fill right shift on a given number.
+ *
+ * This function shifts the bits of the first argument to the right by the number of positions specified
+ * by the second argument, filling the leftmost bits with zeros.
+ *
+ * @param {number} a - The number to be shifted.
+ * @param {number} b - The number of positions to shift.
+ * @returns {number} - The result of the zero-fill right shift operation.
+ */
+function zeroFillRightShift(a, b) {
+    return a >>> b;
+}
+
 module.exports = {
     add,
     subtract,
     multiply,
-    divide
+    divide,
+    exponential,
+    modulus,
+    bitwiseAnd,
+    bitwiseOr,
+    bitwiseXor,
+    bitwiseNot,
+    leftShift,
+    rightShift,
+    zeroFillRightShift
 };
